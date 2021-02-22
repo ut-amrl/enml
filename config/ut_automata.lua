01b800117c4ca6cffc060080a48656f53703705d
Tongrui Li <tli1998@utexas.edu>
2021-02-21 22:56:45 -0600

    tuned enml for new track

--- config/ut_automata.lua
+++ config/ut_automata.lua
@@ -7,32 +7,32 @@ RobotConfig = {
 };
 
 enml = {
-  map_name = "AHG2";
-  starting_loc_x = 114.06;
-  starting_loc_y = -251.86;
-  starting_angle = deg2rad(180.0);
+  map_name = "AHG2_Track";
+  starting_loc_x = 3.0;
+  starting_loc_y = 74.86;
+  starting_angle = deg2rad(-90.0);
 
   -- Hokuyo UST-10lx Sensor parameters.
   laser_std_dev = 0.05;
   min_point_cloud_range = 0.02;
-  max_point_cloud_range = 7.9;
-  max_normal_point_distance = 0.1;
+  max_point_cloud_range = 12.0;
+  max_normal_point_distance = 0.05;
   robot_sensor_offset = vec3(0.22, 0.0, 0.15);
   num_skip_readings = 1;
 
   -- -- Odometry parameters.
   min_rotation = deg2rad(5);
-  min_translation = 0.05;
-  max_odometry_delta_loc = 0.1;
+  min_translation = 0.1;
+  max_odometry_delta_loc = 0.2;
   max_odometry_delta_angle = deg2rad(15.0);
-  odometry_rotation_scale = 1.0;
-  odometry_translation_scale = 1.0;
+  odometry_rotation_scale = 0.9;
+  odometry_translation_scale = 1.1;
 
   -- Parameters for Odometry constraints.
   max_update_period = 0.5;
   odometry_radial_stddev_rate = 0.1;
   odometry_tangential_stddev_rate = 0.1;
-  odometry_angular_stddev_rate = 0.5;
+  odometry_angular_stddev_rate = 0.2;
   odometry_translation_min_stddev = 0.001;
   odometry_translation_max_stddev = 10.5;
   odometry_rotation_min_stddev = deg2rad(10.0);
@@ -40,15 +40,15 @@ enml = {
 
   -- Parameters for LTF constraints.
   map_huber_loss = 0.1;
-  max_point_to_line_distance = 0.25;
+  max_point_to_line_distance = 0.5;
   max_angle_error = deg2rad(35.0);
-  map_correlation_factor = 1.0 / 50.0;
+  map_correlation_factor = 1.0 / 5.0;
 
   -- Parameters for STF constraints.
-  point_match_threshold = 0.15;
-  max_stf_angle_error = deg2rad(25.0);
+  point_match_threshold = 0.0;
+  max_stf_angle_error = deg2rad(35.0);
   max_correspondences_per_point = 6;
-  point_correlation_factor = 1.0 / 40.0;
+  point_correlation_factor = 1.0 / 10.0;
 
   -- Parameters for Object constraints.
   use_object_constraints = false;
@@ -66,8 +66,8 @@ enml = {
 
   -- MLE Optimization parameters.
   pose_increment = 1;
-  max_history = 80;
-  max_solver_iterations = 5;
+  max_history = 5;
+  max_solver_iterations = 20;
   num_repeat_iterations = 1;
   max_repeat_iterations = 4;
   return_jacobian = false;
