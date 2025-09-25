0cd1ffbd0ce430cdb29964677cf4e65096fea0c1
cobot <cobot@robot>
2025-09-25 17:06:13 -0500

    move back to rosbag2_cpp for foxy/humble joint compat

--- src/non_markov_localization_main.cpp
+++ src/non_markov_localization_main.cpp
@@ -41,14 +41,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include "rosbag2_cpp/readers/sequential_reader.hpp"
-#include "rosbag2_storage/storage_options.hpp"
+#include "rosbag2_cpp/storage_options.hpp"
 #include "rosbag2_cpp/converter_interfaces/serialization_format_converter.hpp"
 #include "rclcpp/serialization.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 #include "amrl_msgs/msg/localization2_d_msg.hpp"
 #include "amrl_msgs/msg/visualization_msg.hpp"
@@ -970,7 +970,7 @@ void LoadRosBag(const string& bagName, int max_laser_poses, double time_skip,
 
     // ROS2 rosbag2 setup
     rosbag2_cpp::readers::SequentialReader reader;
-    rosbag2_storage::StorageOptions storage_options;
+    rosbag2_cpp::StorageOptions storage_options;
     storage_options.uri = bagName;
     storage_options.storage_id = "sqlite3";
 
@@ -1780,7 +1780,7 @@ void PlayBagFile(const string& bag_file,
 
     // ROS2 rosbag2 setup
     rosbag2_cpp::readers::SequentialReader reader;
-    rosbag2_storage::StorageOptions storage_options;
+    rosbag2_cpp::StorageOptions storage_options;
     storage_options.uri = bag_file;
     storage_options.storage_id = "sqlite3";
 
